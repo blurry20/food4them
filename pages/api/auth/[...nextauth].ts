973c634fe2401e2c822bc10a3e37bda540ad3e8a
blurry20 <81698530+blurry20@users.noreply.github.com>
2023-06-24 23:04:14 -0400

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,92 +1,72 @@
-import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
-import Credentials from 'next-auth/providers/credentials';
-
-import { dbUsers } from '../../../database';
-
-export default NextAuth({
+import NextAuth, { NextAuthOptions } from "next-auth"
+import Credentials from "next-auth/providers/credentials";
+import GithubProvider from "next-auth/providers/github"
+import { dbUsers } from "../../../database";
+ 
+declare module "next-auth" {
+    interface Session {
+      accessToken?: string;
+    }
+    interface User {
+        id?: string
+        _id: string
+    }
+};
+ 
+export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
-    
     // ...add more providers here
-
     Credentials({
-      name: 'Custom Login',
-      credentials: {
-        email: { label: 'Correo:', type: 'email', placeholder: 'correo@google.com'  },
-        password: { label: 'Contraseña:', type: 'password', placeholder: 'Contraseña'  },
-      },
-      async authorize(credentials) {
-        console.log({credentials})
-        // return { name: 'Juan', correo: 'juan@google.com', role: 'admin' };
-
-        return await dbUsers.checkUserEmailPassword( credentials!.email, credentials!.password );
-
-      }
+        name: 'Custom Login',
+        credentials: {
+            email:{ label:'Correo:', type:'email', placeholder:'correo@google.com' },
+            password: { label:'Contraseña:', type:'password', placeholder:'Contraseña' }
+        },
+        async authorize(credentials) {
+            console.log({credentials});
+            return await dbUsers.checkUserEmailPassword( credentials!.email, credentials!.password );
+        },
     }),
 
 
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+        clientId: process.env.GITHUB_ID!,
+        clientSecret: process.env.GITHUB_SECRET!,
+        
     }),
-
-
   ],
-
-  // Custom Pages
-  pages: {
-    signIn: '/auth/login',
-    newUser: '/auth/register'
-  },
-
-  // Callbacks
-  jwt: {
-    // secret: process.env.JWT_SECRET_SEED, // deprecated
-  },
-  
-  session: {
-    maxAge: 2592000, /// 30d
-    strategy: 'jwt',
-    updateAge: 86400, // cada día
-  },
-
-
+ 
+  //Callbacks
   callbacks: {
-
     async jwt({ token, account, user }) {
-      // console.log({ token, account, user });
-
-      if ( account ) {
-        token.accessToken = account.access_token;
-
-        switch( account.type ) {
-
-          case 'oauth': 
-            token.user = await dbUsers.oAUthToDbUser( user?.email || '', user?.name || '' );
-          break;
-
-          case 'credentials':
-            token.user = user;
-          break;
+        //console.log({ token, account, user });
+        if( account ){
+            token.accessToken = account.access_token;
+ 
+            switch ( account.type ) {
+                case 'oauth':
+                    //TODO: Crear usuario o verificar en Base de datos
+                    break;
+ 
+                case 'credentials':
+                    token.user = user;
+                    break;
+            }
+ 
         }
-
-      }
-
-      return token;
+        return token;
     },
-
-
+ 
     async session({ session, token, user }){
-      // console.log({ session, token, user });
-
-      session.accessToken = token.accessToken;
-      session.user = token.user as any;
-
-      return session;
+        //console.log({ session, token, user });
+        
+        session.accessToken = token.accessToken as any;
+        session.user = token.user as any;
+ 
+        return session;
     }
-    
-
   }
-
-});
+};
+ 
+export default NextAuth(authOptions);
