4c03ce9901f47f97249bafa29e927c7b9b2301e1
blurry20 <81698530+blurry20@users.noreply.github.com>
2023-07-05 14:41:59 -0400

    commit

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -31,10 +31,10 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
 
-  GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
+  //GoogleProvider({
+  //    clientId: process.env.GOOGLE_CLIENT_ID,
+  //    clientSecret: process.env.GOOGLE_CLIENT_SECRET
+  //  })
 
 
   ],
