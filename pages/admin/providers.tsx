979f37dcdfab0a5bacf7eb75adfe048ad7381889
blurry20 <81698530+blurry20@users.noreply.github.com>
2023-07-11 01:14:51 -0400

    provider fix

--- pages/admin/providers.tsx
+++ pages/admin/providers.tsx
@@ -1,116 +1,104 @@
-import { useState, useEffect } from 'react';
-import { PeopleOutline } from '@mui/icons-material'
+import { AddOutlined, PeopleOutline, DeleteOutline } from '@mui/icons-material';
 import useSWR from 'swr';
-
-import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
-import { Grid, Select, MenuItem } from '@mui/material';
-
-import { AdminLayout } from '../../components/layouts'
-import { IUser } from '../../interfaces';
-import { tesloApi } from '../../api';
-
-
-
+import { useState, useEffect } from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Grid, Box, Button, Checkbox } from '@mui/material';
+import { AdminLayout } from '../../components/layouts';
+import { IProvider } from '../../interfaces';
+import { Provider } from '../../models';
+import axios from 'axios';
 
 const ProvidersPage = () => {
+  const { data, error } = useSWR<{ providers: IProvider[] }>('/api/admin/providers');
+  const [providers, setProviders] = useState<IProvider[]>([]);
+  const [selectedRows, setSelectedRows] = useState<string[]>([]);
 
-    const { data, error } = useSWR<IUser[]>('/api/admin/users');
-    const [ users, setUsers ] = useState<IUser[]>([]);
-
-
-    useEffect(() => {
-      if (data) {
-          setUsers(data);
-      }
-    }, [data])
-    
-
-    if ( !data && !error ) return (<></>);
-
-    const onRoleUpdated = async( userId: string, newRole: string ) => {
-
-        const previosUsers = users.map( user => ({ ...user }));
-        const updatedUsers = users.map( user => ({
-            ...user,
-            role: userId === user._id ? newRole : user.role
-        }));
-
-        setUsers(updatedUsers);
-
-        try {
-            
-            await tesloApi.put('/admin/users', {  userId, role: newRole });
-
-        } catch (error) {
-            setUsers( previosUsers );
-            console.log(error);
-            alert('No se pudo actualizar el role del usuario');
-        }
-
+  useEffect(() => {
+    if (data) {
+      setProviders(data.providers);
     }
+  }, [data]);
 
+  if (error) return <div>Error loading providers</div>;
 
-    const columns: GridColDef[] = [
-        { field: 'name', headerName: 'Nombre de Empresa', width: 250},
-        { field: 'contact', headerName: 'Nombre de Contacto', width: 300 },
-        { field: 'email', headerName: 'Correo', width: 250 },
-        { field: 'phone', headerName: 'Telefono', width: 300},
-        { field: 'address', headerName: 'Direccion', width: 300},
-        { field: 'city', headerName: 'Ciudad', width: 300},
-        { field: 'state', headerName: 'País', width: 300},
-        {
-            field: 'role', 
-            headerName: 'Rol', 
-            width: 300,
-            renderCell: ({row}: GridRenderCellParams) => {
-                return (
-                    <Select
-                        value={ row.role }
-                        label="Rol"
-                        onChange={ ({ target }) => onRoleUpdated( row.id, target.value ) }
-                        sx={{ width: '300px' }}
-                    >
-                        <MenuItem value='admin'> Admin </MenuItem>
-                        <MenuItem value='client'> Client </MenuItem>
-                        <MenuItem value='super-user'> Super User </MenuItem>
-                        <MenuItem value='SEO'> SEO </MenuItem>
-                    </Select>
-                )
-            }
-        },
-    ];
+  const columns: GridColDef[] = [
+    {
+      field: 'selection',
+      headerName: '',
+      width: 70,
+      sortable: false,
+      renderCell: (params) => (
+        <Checkbox
+          color="primary"
+          checked={selectedRows.includes(params.row._id)}
+          onChange={() => handleRowSelection(params.row._id)}
+        />
+      ),
+    },
+    { field: 'name', headerName: 'Nombre de Empresa', width: 250 },
+    { field: 'contact', headerName: 'Nombre de Contacto', width: 300 },
+    { field: 'email', headerName: 'Correo', width: 250 },
+    { field: 'phone', headerName: 'Telefono', width: 300 },
+    { field: 'address', headerName: 'Direccion', width: 300 },
+    { field: 'city', headerName: 'Ciudad', width: 300 },
+    { field: 'state', headerName: 'País', width: 300 },
+  ];
 
-    const rows = users.map( user => ({
-        id: user._id,
-        email: user.email,
-        name: user.name,
-        role: user.role
-    }))
+  const rows = providers.map((provider: IProvider, index: number) => ({
+    id: index + 1, // Assign a unique id to each row
+    ...provider,
+  }));
 
+  const handleRowSelection = (providerId?: string) => {
+    if (providerId) {
+      if (selectedRows.includes(providerId)) {
+        setSelectedRows(selectedRows.filter((id) => id !== providerId));
+      } else {
+        setSelectedRows([...selectedRows, providerId]);
+      }
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete('/api/admin/providers', {
+        data: { selectedRows },
+      });
+      const remainingProviders = providers.filter((provider) => !selectedRows.includes(provider._id));
+      setProviders(remainingProviders);
+      setSelectedRows([]);
+    } catch (error) {
+      console.error('Error deleting providers', error);
+    }
+  };
 
   return (
-    <AdminLayout 
-        title={'Proveedores'} 
-        subTitle={'Mantenimiento de proveedores'}
-        icon={ <PeopleOutline /> }
-    >
-
-
-        <Grid container className='fadeIn'>
-            <Grid item xs={12} sx={{ height:650, width: '100%' }}>
-                <DataGrid 
-                    rows={ rows }
-                    columns={ columns }
-                    pageSize={ 10 }
-                    rowsPerPageOptions={ [10] }
-                />
-
-            </Grid>
+    <AdminLayout title={'Proveedores'} subTitle={'Mantenimiento de proveedores'} icon={<PeopleOutline />}>
+      <Box display="flex" justifyContent="end" sx={{ mb: 2 }}>
+        <Button startIcon={<AddOutlined />} color="secondary" href="/admin/providers/new">
+          Crear proveedor
+        </Button>
+        <Button
+          startIcon={<DeleteOutline />}
+          color="error"
+          onClick={handleDelete}
+          disabled={selectedRows.length === 0}
+        >
+          Eliminar
+        </Button>
+      </Box>
+      <Grid container className="fadeIn">
+        <Grid item xs={12} sx={{ height: 650, width: '100%' }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={10}
+            rowsPerPageOptions={[10]}
+          />
         </Grid>
-
-
+      </Grid>
     </AdminLayout>
-  )
-}
+  );
+};
 
-export default ProvidersPage
+export default ProvidersPage;
