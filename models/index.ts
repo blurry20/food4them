60bce1eff097f95efb5d90afb916b1f0bcee0eb8
blurry20 <81698530+blurry20@users.noreply.github.com>
2023-07-11 00:49:15 -0400

    update

--- models/index.ts
+++
@@ -1,7 +0,0 @@
-import Provider from './Provider';
-
-
-export {default as Provider}  from  './Provider'
-export { default as Product } from './Product';
-export { default as Order } from './Order';
-export { default as User } from './User';
